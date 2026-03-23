34a0df19fa3cc950372c5de92cb54dd761b1501b
mtabassum <mtabassum015@gmail.com>
2026-03-23 14:35:07 -0500

    refactor(ui): move villa detail into partial view

---
+++ ResortManagement.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using ResortManagement.Domain.Entities;
+
+namespace ResortManagement.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
